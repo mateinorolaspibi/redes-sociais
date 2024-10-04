e3446f4d5b18d59aa9555d68cc89d330eff1b68a
mateinorolaspibi <127767299+mateinorolaspibi@users.noreply.github.com>
2024-10-04 10:47:22 -0300

    Add files via upload

---
+++ redes-sociais-main/graficos/informacoesGlobais.js
@@ -0,0 +1,10 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualuizarInformacoesGlobais(){
+     const res = await fetch(url)
+     const dados = await res.json()
+   const paragrafo =document.createElement('')
+}
+
+vizualuizarInformacoesGlobais ()
+
