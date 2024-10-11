46c3a91dbc0b01a4012a10858b4a82475bfb70d3
mateinorolaspibi <127767299+mateinorolaspibi@users.noreply.github.com>
2024-10-11 10:51:32 -0300

    Add files via upload

--- redes-sociais-main/graficos/informacoesGlobais.js
+++ redes-sociais-main/graficos/informacoesGlobais.js
@@ -3,13 +3,18 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualuizarInformacoesGlobais(){
      const res = await fetch(url)
      const dados = await res.json()
-     console.log(dados);
+     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+     const horas = perseInt(dados.tempo_medio)
+     const minutos = Math.round((dados.tempo_medio - horas) * 100)
+     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+
    const paragrafo = document.createElement('p')
    paragrafo.classList.add('graficos-container_texto')
-
-   paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passamem média ${dados.tempo_medio} horas conectadas.`
-   console.log(paragrafo)
-
+   paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span>
+    de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão
+     conectadas em alguma rede social e passamem média <pan>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <pan>${porcentagemConectada}%</span> de pessoas estão conctadas em alguma rede social `
+  
    const container = document.getElementById('graficos-container')
    container.appendChild(paragrafo)
 }
